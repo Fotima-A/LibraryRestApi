8013cb68852c8a99be12ae5bcb4d9a163acd0a85
Fotima-A <fgiyosiddinova@gmail.com>
2025-05-23 10:16:15 +0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -35,16 +35,15 @@ class User(AbstractUser):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    quantity = models.IntegerField(default=1)  # quantity maydoni qo‘shildi
-    daily_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  # OrderReturnView da ishlatilgan
-
+    quantity = models.IntegerField(default=1)  
+    daily_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)  
     def __str__(self):
         return self.title
 
 class Order(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
-    order_date = models.DateTimeField(default=timezone.now)  # order_date maydoni qo‘shildi
+    order_date = models.DateTimeField(default=timezone.now)  
     status = models.CharField(max_length=20, choices=OrderStatus.choices, default=OrderStatus.BOOKED)
     taken_at = models.DateTimeField(null=True, blank=True)
     returned_at = models.DateTimeField(null=True, blank=True)
