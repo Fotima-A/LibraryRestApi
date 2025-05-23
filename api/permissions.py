ee0f619f66b2b12bfce18358ee16e0028dfd60a8
Fotima-A <fgiyosiddinova@gmail.com>
2025-05-23 10:16:35 +0500

    Update permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -6,11 +6,9 @@ class RoleBasedPermission(permissions.BasePermission):
         self.allowed_roles = allowed_roles if allowed_roles is not None else []
 
     def has_permission(self, request, view):
-        # Agar foydalanuvchi autentifikatsiyalanmagan bo‘lsa, ruxsat yo‘q
         if not request.user.is_authenticated:
             return False
 
-        # Admin har doim barcha ruxsatlarga ega
         if request.user.role == UserRole.ADMIN:
             return True
 
@@ -21,7 +19,6 @@ class RoleBasedPermission(permissions.BasePermission):
         return user_role in [role.value for role in self.allowed_roles]
 
     def has_object_permission(self, request, view, obj):
-        # Obyekt darajasidagi ruxsatni ham admin uchun har doim beramiz
         if request.user.is_authenticated and request.user.role == UserRole.ADMIN:
             return True
         return self.has_permission(request, view)
